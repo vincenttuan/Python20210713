3e5a77f42230e4d69e9762d5d9339d10b7b0be92
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-17 19:27:21 +0800

    Add files via upload

---
+++ day12/emp/CreateTables.py
@@ -0,0 +1,40 @@
+'''
+建立資料表
+1. 部門 departments
++---------+-----------+
+| dept_id | dept_name |
++---------+-----------+
+| INTEGER | VARCHAR   |
++---------+-----------+
+|   PK    | NOT NULL  |
++---------+-----------+
+
+2. 員工 employees
++--------+---------+----------+------------+-------------+
+| emp_id | dept_id | emp_name | emp_salary | create_date |
++--------+---------+----------+------------+-------------+
+| INTEGER| INTEGER | VARCHAR  |  INTEGER   |  TIMESTAMP  |
++--------+---------+----------+------------+-------------+
+|   PK   |   FK    | NOT NULL |            |   DEFAULT   |
++--------+---------+----------+------------+-------------+
+
+'''
+
+create_departments_sql = '''
+    CREATE TABLE departments(
+        dept_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dept_name VARCHAR NOT NULL
+    );
+'''
+
+create_employees_sql = '''
+    CREATE TABLE employees(
+        emp_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dept_id INTEGER,
+        emp_name VARCHAR NOT NULL,
+        emp_salary INTEGER,
+        create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT fk_departments FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+    );
+'''
+
