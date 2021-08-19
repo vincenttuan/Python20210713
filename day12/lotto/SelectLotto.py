efdca7e5b38d2e1b5f59e2e58540525a3bdbff04
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-19 19:25:46 +0800

    Add files via upload

---
+++ day12/lotto/SelectLotto.py
@@ -0,0 +1,15 @@
+# 在 10000 期中, 每一個數字所出現的次數為何?
+import sqlite3
+
+conn = sqlite3.connect('lotto.db')
+cursor = conn.cursor()
+
+for i in range(49):
+    n = i+1
+    sql = "select count(*) as count from lotto " \
+          "where n1=%d or n2=%d or n3=%d or n4=%d or n5=%d or n6=%d" % (n, n, n, n, n, n)
+    cursor.execute(sql)
+    count = cursor.fetchone()
+    print(n, count[0])
+
+conn.close()
