3462eb145a154978e53e8e31f802a74d85797d36
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-24 18:44:51 +0800

    Add files via upload

--- lab/ttt/TicTaoToc2.py
+++ lab/ttt/TicTaoToc2.py
@@ -11,17 +11,31 @@ def printTTT():
     for row in ttt:
         print(row)
 
+
+def keepGoing():
+    for row in ttt:
+        for value in row:
+            if value == ' ':
+                return True
+    return False
+
+
 def play():
     printTTT()
     # 使用者玩「O」
+    if not keepGoing():
+        return False
     n = int(input('請輸入位置（0~8）：'))
 
     if n == -1:
         return False  # 離開
 
     ttt[n // 3][n % 3] = 'O'
+    printTTT()
 
     # 電腦玩「X」
+    if not keepGoing():
+        return False
     print('電腦計算中...')
     t.sleep(2)
     while True:
