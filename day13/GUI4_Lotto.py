2453bc576062a563cb1f9b3376a2be8f5e81c118
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-24 21:36:49 +0800

    Add files via upload

--- day13/GUI4_Lotto.py
+++ day13/GUI4_Lotto.py
@@ -1,5 +1,8 @@
+import random
 import tkinter
 from tkinter import font
+import threading
+import time
 '''
 +---+---+---+---+
 | 2 | 3 | 1 | 7 |
@@ -7,16 +10,30 @@ from tkinter import font
 |  Play |  Exit |
 +-------+-------+
 '''
+def generateValue1():
+    for i in range(30):
+        n = random.randint(0, 9)
+        value1.set(n)
+        time.sleep(0.05)
+
+def play():
+    t1 = threading.Thread(target=generateValue1)
+    t1.start()
+
+
 win = tkinter.Tk()
 win.title("我的小視窗 4")
 win.geometry("500x250")
 
+value1 = tkinter.IntVar()
+value1.set(0)
+
 myfont = font.Font(family='Arial', size=36, weight='bold')
-n1 = tkinter.Button(text='2', font=myfont)
+n1 = tkinter.Button(textvariable=value1, font=myfont)
 n2 = tkinter.Button(text='3', font=myfont)
 n3 = tkinter.Button(text='1', font=myfont)
 n4 = tkinter.Button(text='7', font=myfont)
-b1 = tkinter.Button(text='Play', font=myfont)
+b1 = tkinter.Button(text='Play', font=myfont, command=play)
 b2 = tkinter.Button(text='Exit', font=myfont)
 
 win.rowconfigure((0, 1), weight=1) # 列 0, 1 同步放大與縮小
