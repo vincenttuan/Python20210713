40dca90ff33312a8b4feed6b0cd5f9197fc4f404
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-24 19:06:44 +0800

    Add files via upload

--- lab/ttt/TicTaoToc2.py
+++ lab/ttt/TicTaoToc2.py
@@ -19,11 +19,34 @@ def keepGoing():
                 return True
     return False
 
+def theWinnerIs():
+    if ttt[0][0] + ttt[0][1] + ttt[0][2] == 'OOO' or \
+       ttt[1][0] + ttt[1][1] + ttt[1][2] == 'OOO' or \
+       ttt[2][0] + ttt[2][1] + ttt[2][2] == 'OOO' or \
+       ttt[0][0] + ttt[1][0] + ttt[2][0] == 'OOO' or \
+       ttt[0][1] + ttt[1][1] + ttt[2][1] == 'OOO' or \
+       ttt[0][2] + ttt[1][2] + ttt[2][2] == 'OOO' or \
+       ttt[0][0] + ttt[1][1] + ttt[2][2] == 'OOO' or \
+       ttt[0][2] + ttt[1][1] + ttt[2][0] == 'OOO' :
+        print('使用者贏')
+        return True
+
+    if ttt[0][0] + ttt[0][1] + ttt[0][2] == 'XXX' or \
+       ttt[1][0] + ttt[1][1] + ttt[1][2] == 'XXX' or \
+       ttt[2][0] + ttt[2][1] + ttt[2][2] == 'XXX' or \
+       ttt[0][0] + ttt[1][0] + ttt[2][0] == 'XXX' or \
+       ttt[0][1] + ttt[1][1] + ttt[2][1] == 'XXX' or \
+       ttt[0][2] + ttt[1][2] + ttt[2][2] == 'XXX' or \
+       ttt[0][0] + ttt[1][1] + ttt[2][2] == 'XXX' or \
+       ttt[0][2] + ttt[1][1] + ttt[2][0] == 'XXX' :
+        print('電腦贏')
+        return True
+
+    return False
 
 def play():
-    printTTT()
     # 使用者玩「O」
-    if not keepGoing():
+    if not keepGoing():  # 是否有空間可以繼續玩 ?
         return False
     n = int(input('請輸入位置（0~8）：'))
 
@@ -31,10 +54,12 @@ def play():
         return False  # 離開
 
     ttt[n // 3][n % 3] = 'O'
-    printTTT()
+    printTTT()  # 印出盤面
+    if theWinnerIs():  # 判斷是否有贏家
+        return False
 
     # 電腦玩「X」
-    if not keepGoing():
+    if not keepGoing():  # 是否有空間可以繼續玩 ?
         return False
     print('電腦計算中...')
     t.sleep(2)
@@ -44,9 +69,14 @@ def play():
             ttt[n // 3][n % 3] = 'X'
             break
 
+    printTTT()  # 印出盤面
+    if theWinnerIs():  # 判斷是否有贏家
+        return False
+
     return True  # 繼續玩
 
 if __name__ == '__main__':
+    printTTT()  # 印出盤面
     while play():
         pass
 
