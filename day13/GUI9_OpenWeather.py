17f8ad24ceb839a18187986cc184b9e9306d80b0
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-26 20:36:12 +0800

    Add files via upload

--- day13/GUI9_OpenWeather.py
+++ day13/GUI9_OpenWeather.py
@@ -16,36 +16,52 @@ OpenWeather
 '''
 
 import tkinter
+import requests
+import json
 from tkinter import font
 
-win = tkinter.Tk()
-win.title('Open Weather')
-win.geometry('500x200')
+def openweatherService(q):
+    path = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s' % (q, appid)
+    ow = json.loads(requests.get(path).text)
+    print(ow)
 
-myfont = font.Font(family='Arial', size=24, weight='bold')
+def submit():
+    q = entry.get()
+    openweatherService(q)
 
-entry = tkinter.Entry(font=myfont, justify=tkinter.CENTER)
-submit_btn = tkinter.Button(text='Submit', font=myfont)
 
-t_label = tkinter.Label(text='temperature', font=myfont)
-t_result = tkinter.Label(text='0.0 °C', font=myfont)
+if __name__ == '__main__':
+    appid = "ab99f5242005a98291f643ea873b363d"
 
-h_label = tkinter.Label(text='humidity', font=myfont)
-h_result = tkinter.Label(text='0.0 %', font=myfont)
+    win = tkinter.Tk()
+    win.title('Open Weather')
+    win.geometry('500x200')
 
-desp_label = tkinter.Label(text='?', font=myfont)
-icon_label = tkinter.Label(text='?', font=myfont)
+    myfont = font.Font(family='Arial', size=24, weight='bold')
 
-win.rowconfigure((0, 1), weight=1)  # 列 0, 1 同步放大與縮小
-win.columnconfigure((0, 1), weight=1)  # 欄 0, 1 同步放大與縮小
+    entry = tkinter.Entry(font=myfont, justify=tkinter.CENTER)
+    entry.insert(0, 'taoyuan,tw')
+    submit_btn = tkinter.Button(text='Submit', font=myfont, command=submit)
 
-entry.grid(row=0, column=0, columnspan=1, sticky='EWNS')
-submit_btn.grid(row=0, column=1, columnspan=1, sticky='EWNS')
-t_label.grid(row=1, column=0, columnspan=1, sticky='EWNS')
-t_result.grid(row=1, column=1, columnspan=1, sticky='EWNS')
-h_label.grid(row=2, column=0, columnspan=1, sticky='EWNS')
-h_result.grid(row=2, column=1, columnspan=1, sticky='EWNS')
-desp_label.grid(row=3, column=0, columnspan=2, sticky='EWNS')
-icon_label.grid(row=4, column=0, columnspan=2, sticky='EWNS')
+    t_label = tkinter.Label(text='temperature', font=myfont)
+    t_result = tkinter.Label(text='0.0 °C', font=myfont)
 
-win.mainloop()
+    h_label = tkinter.Label(text='humidity', font=myfont)
+    h_result = tkinter.Label(text='0.0 %', font=myfont)
+
+    desp_label = tkinter.Label(text='?', font=myfont)
+    icon_label = tkinter.Label(text='?', font=myfont)
+
+    win.rowconfigure((0, 1), weight=1)  # 列 0, 1 同步放大與縮小
+    win.columnconfigure((0, 1), weight=1)  # 欄 0, 1 同步放大與縮小
+
+    entry.grid(row=0, column=0, columnspan=1, sticky='EWNS')
+    submit_btn.grid(row=0, column=1, columnspan=1, sticky='EWNS')
+    t_label.grid(row=1, column=0, columnspan=1, sticky='EWNS')
+    t_result.grid(row=1, column=1, columnspan=1, sticky='EWNS')
+    h_label.grid(row=2, column=0, columnspan=1, sticky='EWNS')
+    h_result.grid(row=2, column=1, columnspan=1, sticky='EWNS')
+    desp_label.grid(row=3, column=0, columnspan=2, sticky='EWNS')
+    icon_label.grid(row=4, column=0, columnspan=2, sticky='EWNS')
+
+    win.mainloop()
