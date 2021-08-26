98c8b19014f037e2c83d9b30aa54bf0e190bc5ad
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-26 18:36:13 +0800

    Add files via upload

--- day13/GUI6_MessageBox.py
+++ day13/GUI6_MessageBox.py
@@ -3,18 +3,22 @@ from tkinter import messagebox as msg
 
 def show_info():
     r = msg.showinfo('showinfo', 'This is [showinfo] dialog')
+    result.set(r)
     print(r)
 
 def show_askokcancel():
     r = msg.askokcancel('askokcancel', 'This is [askokcancel] dialog')
+    result.set(r)
     print(r)
 
 def show_askyesno():
     r = msg.askyesno('askyesno', 'This is [askyesno] dialog')
+    result.set(r)
     print(r)
 
 def show_askyesnocancel():
     r = msg.askyesnocancel('show_askyesnocancel', 'This is [askyesnocancel] dialog')
+    result.set(r)
     print(r)
 
 
@@ -33,5 +37,11 @@ b3.pack()
 
 b4 = tkinter.Button(win, text="show_askyesnocancel", command=show_askyesnocancel)
 b4.pack()
+
+result = tkinter.StringVar()
+result.set("?")
+label = tkinter.Label(win, textvariable=result, font=('Arial', 100))
+label.pack()
+
 
 win.mainloop()
