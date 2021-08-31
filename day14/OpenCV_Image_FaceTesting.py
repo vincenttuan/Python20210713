3b98335bc49de5d1aa38b9f226974816fc937ead
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-31 19:16:52 +0800

    Add files via upload

--- day14/OpenCV_Image_FaceTesting.py
+++ day14/OpenCV_Image_FaceTesting.py
@@ -6,8 +6,22 @@ face_cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
 # 讀檔
 frame = cv2.imread('./image/test.jpg')
 
+# 將彩色圖片(RGB)進行灰階(Gray)處理
+gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+# face 的座標範圍
+faces = face_cascade.detectMultiScale(
+    gray,  # 待檢測圖片
+    scaleFactor=1.1,  # 檢測粒度(越小越精準(速度慢), 越大越模糊(速度快))
+    minNeighbors=5,  # 每個目標至少檢測到幾次成功，才被認定是 face
+    minSize=(30, 30),  # 設定數據最小搜尋的尺寸
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print(faces)
+
 # 將 frame 顯示
-cv2.imshow('My window', frame)
+cv2.imshow('My window', gray)
 
 # 按下任意鍵離開程式
 c = cv2.waitKey(0)
